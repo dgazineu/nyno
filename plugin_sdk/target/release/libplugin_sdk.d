7865c92bad696ee33884502595af3dde5463daa4
user <user@staryoga>
2026-04-29 23:35:51 +0200

    7.0.1: fixed plugin sdk for Rust

--- plugin_sdk/target/release/libplugin_sdk.d
+++ plugin_sdk/target/release/libplugin_sdk.d
@@ -1,1 +1,1 @@
-/home/user/nyno/plugin_sdk/target/release/libplugin_sdk.rlib: /home/user/nyno/plugin_sdk/src/lib.rs
+/home/user/nyno-nightly/nyno-private-extensions/plugin_sdk/target/release/libplugin_sdk.rlib: /home/user/nyno-nightly/nyno-private-extensions/plugin_sdk/src/lib.rs
